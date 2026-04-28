eda48c1728e0c818cb696e6d5a1945b0cdebf605
devarshpc <devarshpc@gmail.com>
2026-04-28 19:55:34 +0530

    created utils for household schema

---
+++ backend/src/utils/inviteCode.ts
@@ -0,0 +1,11 @@
+
+
+export const generateInviteCode = () => {
+    const chars = "ABCDEGHIJKLMNOPQRSTUVWXYZ0123456789"
+    let code = "";
+    for(let i=0; i<6; i++)
+    {
+        code += chars[Math.floor(Math.random() * chars.length)];
+    }
+    return code;
+}
